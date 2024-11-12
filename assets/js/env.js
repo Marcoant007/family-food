ee25e600cee765fbeb157ce7862baa5fce2e570b
marco.ajunior <marco.ajunior@luizalabs.com>
2024-11-12 10:53:57 -0300

    fix: update envs

--- assets/js/env.js
+++ assets/js/env.js
@@ -1,5 +1,5 @@
 window.env = {
-    IMGBB_API_KEY: '795ec2b2595141a77a60bd454b61e2c5',
+    STRINGF: '795ec2b2595141a77a60bd454b61e2c5',
     // TELEFONE_WHATSAPP:'+5527998360839',
     TELEFONE_WHATSAPP: '+5527988740756',
     PIX_CODE: 'familiaalicerce29@gmail.com'
